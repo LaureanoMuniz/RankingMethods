3eef52aa20b6ad08e45556644e244402b7a71e16
Carlos Miguel Soto <csoto@dc.uba.ar>
2021-04-02 23:56:45 -0300

    limpiamos el código

--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -1,125 +1,197 @@
 #include <iostream>
 #include <fstream>
+#include <istream>
 #include <sstream>
 #include <vector>
+#include <map>
+#include <string>
+#include <cassert>
+
 using namespace std;
 using mat = vector<vector<double>>;
+template<typename T>
+struct Mat {
+    int n, m;
+	vector<vector<T>> mat;
+    T &operator()(int i, int j = 0) {
+        return mat[i][j];  
+    }
+    const T &operator()(int i, int j = 0) const {
+        return mat[i][j];  
+    }
 
-mat readData0(string path){
-	ifstream infile(path);
-
-	int equipos,partidos;
-	infile >> equipos >> partidos;
-	mat result(equipos, vector<double>(equipos + 1,0));
-	for (int i = 0; i < equipos; ++i)
-	{
-		//inicalizo la diagonal y el vector B antes de acumular
-		result[i][i] = 2;
-		result[i][equipos] = 1;
-	}
-
-	for (int i = 0; i < partidos; ++i)
-	{
-		//generamos el sistema acumulando los valores a medida que leemos
-		int identificador,equipo1,equipo2,puntaje1,puntaje2;
-		infile >> identificador >> equipo1 >> puntaje1 >> equipo2 >> puntaje2;
-		if (puntaje1 >= puntaje2){
-			result[equipo1 - 1][equipos] += 0.5;
-			result[equipo2 - 1][equipos] -= 0.5;
-		}else{
-			result[equipo1 - 1][equipos] -= 0.5;
-			result[equipo2 - 1][equipos] += 0.5;
+	static Mat cero(int _n, int _m = 1) {
+        Mat res { _n, _m, vector<vector<T>>(_n, vector<T>(_m, 0)) };
+		return res;
+    }
+	static Mat identidad(int _n) {
+        Mat res = Mat::cero(_n, _n);
+        for(int i = 0; i < res.n; i++){
+			res(i, i) = 1;
 		}
-		result[equipo1 - 1][equipo2 - 1] -= 1;
-		result[equipo2 - 1][equipo1 - 1] -= 1;
-		result[equipo1 - 1][equipo1 - 1] += 1;
-		result[equipo2 - 1][equipo2 - 1] += 1;
+		return res;
+    }
+	void operacion_2(int fila_1, int fila_2, T multiplo){
+		for(int i = 0; i < m; i++){
+			mat[fila_2][i] = mat[fila_2][i] + mat[fila_1][i] * multiplo;
+		}
 	}
+	void operacion_3(int fila_1, int fila_2){
+		swap(mat[fila_1], mat[fila_2]);
+	}
+};
 
-	return(result);
-	//todo
+struct Partido {
+    string fecha;
+    int puntos_i;
+    int id_i;
+    int puntos_j;
+    int id_j;
+};
+
+struct Torneo {
+    vector<string> nombres;
+    vector<Partido> partidos;
+    int equipos() const {
+        return int(nombres.size());
+    }
+};
+
+template<typename FDescr>
+Torneo read_data(FDescr &file) {
+    int equipos, partidos;
+    file >> equipos >> partidos;
+    std::map<string, int> ids_equipos;
+
+    Torneo torneo;
+
+    for(int i = 0; i < partidos; ++i) {
+        string equipo_i;
+        string equipo_j;
+        Partido partido;
+        file >> partido.fecha >> equipo_i >> partido.puntos_i >> equipo_j >> partido.puntos_j;
+        for(auto name : {equipo_i, equipo_j}) {
+            if(ids_equipos.find(name) == ids_equipos.end()) {
+                int k = int(ids_equipos.size());
+                torneo.nombres.push_back(name);
+                ids_equipos[name] = k;
+            }
+        }
+        partido.id_i = ids_equipos[equipo_i];
+        partido.id_j = ids_equipos[equipo_j];
+        torneo.partidos.push_back(partido);
+    }
+    return torneo;
 }
 
+pair<Mat<double>, Mat<double>> ganados_y_perdidos(const Torneo &torneo) {
+    auto ganados = Mat<double>::cero(torneo.equipos());
+    auto perdidos = Mat<double>::cero(torneo.equipos());
+    
+    for(const auto &partido : torneo.partidos) {
+        int ganador, perdedor;
+        if(partido.puntos_i > partido.puntos_j) {
+            ganador = partido.id_i;
+            perdedor = partido.id_j;
+        } else if(partido.puntos_i < partido.puntos_j) {
+            ganador = partido.id_j;
+            perdedor = partido.id_i;
+        } else assert(false);
+        ganados(ganador) += 1;
+        perdidos(perdedor) += 1;
+    }
+    return {ganados, perdidos};
+}
 
-vector<double> eliminacionGaussiana(mat & sistema){
-	for (int i = 0; i < sistema.size() - 1; ++i)
-	{
-		for (int j = i + 1; j < sistema.size(); j++)
-		{
-			double coef = sistema[j][i] / sistema[i][i];
+Mat<double> sistema_CMM(const Torneo &torneo){
+    auto jugados = Mat<double>::cero(torneo.equipos(), torneo.equipos());
+    
+    for(const auto &partido : torneo.partidos) {
+        jugados(partido.id_i, partido.id_j) += 1;
+        jugados(partido.id_j, partido.id_i) += 1;
+    }
 
-			for (int k = i; k < sistema[0].size(); k++)
-			{
-				sistema[j][k] = sistema[j][k] - (sistema[i][k] * coef);
-			}
+    auto [ganados, perdidos] = ganados_y_perdidos(torneo);
+
+    auto sistema = Mat<double>::cero(torneo.equipos(), torneo.equipos()+1);
+    for(int i = 0; i < torneo.equipos(); ++i) {
+        for(int j = 0; j < torneo.equipos(); ++j) {
+            if(i != j) sistema(i, j) = -jugados(i, j);
+            else sistema(i, j) = 2.0 + ganados(i) + perdidos(i);
+        }
+        sistema(i, torneo.equipos()) = 1.0 + (ganados(i) - perdidos(i)) / 2.0;
+    }
+    
+	return sistema;
+}
+
+template<typename T>
+auto eliminacion_gaussiana(Mat<T> sistema) -> Mat<T> {
+	for (int i = 0; i < sistema.n; ++i){
+		assert(sistema(i,i) != 0);
+		for (int j = i + 1; j < sistema.n; j++){
+			T coef = sistema(j, i) / sistema(i, i);
+			sistema.operacion_2(i, j, -coef);
 		}
 	}
-	vector<double> result(sistema.size(),0);
-	for (int i = result.size() -1; i >= 0; i--)
-	{
-		double acum = 0;
-		for (int j = i + 1; j < sistema.size(); j++)
-		{
-			acum += ( result[j] *  sistema[i][j]);
+	auto result = Mat<T>::cero(sistema.n);
+	for (int i = sistema.n - 1; i >= 0; i--){
+		T acum = 0;
+		for (int j = i + 1; j < sistema.n; j++){
+			acum += ( result(j) *  sistema(i, j) );
 		}
-		result[i] = ( sistema[i][sistema.size()] - acum ) / sistema[i][i];
+		result(i) = ( sistema(i, sistema.n) - acum ) / sistema(i, i);
 	}
-
 	return result;
-
 }
 
-void printRankings(vector<double> & rankings, string path){
+void print_rankings(const Torneo &torneo, const Mat<double> & rankings, string path){
 	ofstream file(path);
-	for (int i = 0; i < rankings.size(); ++i)
+	for (int i = 0; i < torneo.equipos(); ++i)
 	{
-		file << rankings[i] << endl;
+		file << torneo.nombres[i] << " " << rankings(i) << endl;
 	}
 }
 
-vector<double> readData1(string path){
-	ifstream infile(path);
-	int equipos,partidos;
-	infile >> equipos >> partidos;
+Mat<double> WP(const Torneo &torneo){
 
-	vector<double> res(equipos,0);
-	vector<double> partidos_por_equipo(equipos, 0);
-
-	for (int i = 0; i < partidos; ++i)
+	auto result = Mat<double>::cero(torneo.equipos()); 
+	auto [ganados, perdidos] = ganados_y_perdidos(torneo);
+	for (int i = 0; i < torneo.equipos(); ++i)
 	{
-		int identificador,equipo1,equipo2,puntaje1,puntaje2;
-		infile >> identificador >> equipo1 >> puntaje1 >> equipo2 >> puntaje2;
-
-		if(puntaje1 > puntaje2){
-			res[equipo1 - 1] ++;
-		}else{
-			res[equipo2 - 1] ++;
-		}
-		partidos_por_equipo[equipo1 - 1] ++;
-		partidos_por_equipo[equipo2 - 1] ++;
+		result(i) = ganados(i)/(ganados(i)+perdidos(i));
 	}
-	for (int i = 0; i < equipos; ++i)
-	{
-		res[i] = res[i]/partidos_por_equipo[i];
-	}
-	return res;
+	return result;
 }
 
+void display_usage() {
+    cout << R"(uso: ./tp1 algorithm source destination
+algorithm:
+    debe ser 0, 1 o 2
+source:
+    path al archivo .tsv del que se leerá la entrada
+destination:
+    path al archivo donde se escribirá la salida
+)";
+    exit(0);
+}
  
 int main(int argc, char **argv){
-	vector<double> rankings;
-    if (*argv[1] == '0'){
-		mat sistema = readData0(argv[2]);
-		rankings = eliminacionGaussiana(sistema);	
+    if(argc != 4) display_usage();
+	ifstream file(argv[2]);
+	Torneo torneo = read_data(file);
+    Mat<double> rankings;
+    if (string(argv[1]) == "0"){
+		rankings = eliminacion_gaussiana(sistema_CMM(torneo));	
 	}
-	else if(*argv[1] == '1'){
-		rankings = readData1(argv[2]);
+	else if(string(argv[1]) == "1"){
+		rankings = WP(torneo);
 	}
-	else if(*argv[1] == '2'){
+	else if(string(argv[1]) == "2"){
 		//todo
 	}else{
 		cout << "Uso: metodo, inputpath, outputpath" <<  endl;
 	}
-	printRankings(rankings,argv[3]);
+	print_rankings(torneo, rankings, argv[3]);
 	return 0;
 }
